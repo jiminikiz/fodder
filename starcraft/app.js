2db666810a27c1799e36e8bc6e7fdfea2681a771
Brandon Jimenez <jiminikiz@gmail.com>
2016-08-30 13:11:03 -0600

    Removing the dead

--- starcraft/app.js
+++
@@ -1,6 +0,0 @@
-angular.module('starcraft', [ ]);
-
-angular.module('starcraft')
-    .controller('Pagination', function(){
-        this.buttons = [1,2,3,4,5];
-    });
--- test/test.js
+++
@@ -1,5 +0,0 @@
-function random(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-}
-
-console.log(random(1,10));
